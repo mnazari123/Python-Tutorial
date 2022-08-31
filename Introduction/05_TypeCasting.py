29ab428746a84b84d8e852f277fc59e69e998a08
mnazari123 <mnazari@luc.edu>
2022-08-31 12:45:28 -0500

    Introduction Section 02 commit

---
+++ Introduction/05_TypeCasting.py
@@ -0,0 +1,17 @@
+# Type Casting is the ability to convert the data types of a value to another data type
+
+x = 1 # int
+y = 2.0 # float; 
+z = "3" # string 
+
+# int(y) adding the type before the data and adding the data inside the paranthesis 
+# float(x), float(z) 
+# str(x), str(z)
+
+print (x)
+print (int(y))
+print (int(z)*3)
+
+
+# it is important when a string type is printed next to float or intiger type
+# we need to convert first then it will be printable. 
